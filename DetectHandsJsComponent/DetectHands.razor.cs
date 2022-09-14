b34714640fccb855fb99f9bac0fd5c8373d66f50
pavelsavara <pavel.savara@gmail.com>
2022-09-14 16:11:12 +0200

    initial

---
+++ DetectHandsJsComponent/DetectHands.razor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.InteropServices.JavaScript;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Text.Json.Serialization.Metadata;
+using System.Diagnostics.CodeAnalysis;
+
+namespace DetectHandsJsComponent
+{
+    public partial class DetectHands
+    {
+        protected override async Task OnInitializedAsync()
+        {
+            await JSHost.ImportAsync("DetectHandsJsComponent/DetectHands", "/_content/DetectHandsJsComponent/DetectHands.razor.js");
+            await Interop.OnInit();
+        }
+
+        public partial class Interop
+        {
+            [DynamicDependency(DynamicallyAccessedMemberTypes.PublicMethods, typeof(JsonTypeInfo))]
+            [DynamicDependency(DynamicallyAccessedMemberTypes.PublicMethods, typeof(JsonSerializerContext))]
+            static Interop()
+            {
+            }
+
+            [JSImport("onInit", "DetectHandsJsComponent/DetectHands")]
+            internal static partial Task OnInit();
+
+            [JSExport]
+            internal static void OnResults(string json)
+            {
+                Console.WriteLine("OnResults");
+                //return JsonSerializer.Deserialize<List<Item>>(json) ?? new List<Item>();
+            }
+        }
+    }
+}
---
+++ DetectHandsJsComponent/DetectHands.razor.js
@@ -0,0 +1,86 @@
+﻿// Copyright 2020 The MediaPipe Authors.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const mpHands = window;
+const drawingUtils = window;
+const videoElement = document.getElementsByClassName('input_video')[0];
+const canvasElement = document.getElementsByClassName('output_canvas')[0];
+const canvasCtx = canvasElement.getContext('2d');
+
+async function insertGlobalScript(url) {
+    var element = document.createElement('script');
+    element.setAttribute('src', url);
+    element.setAttribute('crossorigin', 'anonymous');
+    document.head.appendChild(element);
+
+    return new Promise((resolve) => {
+        element.onload = () => {
+            resolve();
+        }; 
+    });
+}
+
+export async function onInit() {
+    console.log("onInit");
+    await insertGlobalScript('https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils@0.3/camera_utils.js')
+    await insertGlobalScript('https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils@0.3/drawing_utils.js')
+    await insertGlobalScript('https://cdn.jsdelivr.net/npm/@mediapipe/hands@0.4/hands.js')
+
+    const hands = new mpHands.Hands({
+        locateFile: (file) => {
+            return `https://cdn.jsdelivr.net/npm/@mediapipe/hands@${mpHands.VERSION}/${file}`;
+        }
+    });
+    hands.setOptions({
+        selfieMode: true,
+        maxNumHands: 2,
+        modelComplexity: 0,
+        minDetectionConfidence: 0.5,
+        minTrackingConfidence: 0.5
+    });
+    hands.onResults(onResults);
+    const camera = new Camera(videoElement, {
+        onFrame: async () => {
+            await hands.send({ image: videoElement });
+        },
+        width: 1280,
+        height: 720
+    });
+    camera.start();
+    console.log("camera.started");
+}
+
+function onResults(results) {
+    if (results.multiHandedness.length) console.log(JSON.stringify(results.multiHandedness, null, 2))
+    // Draw the overlays.
+    canvasCtx.save();
+    canvasCtx.clearRect(0, 0, canvasElement.width, canvasElement.height);
+    canvasCtx.drawImage(results.image, 0, 0, canvasElement.width, canvasElement.height);
+    if (results.multiHandLandmarks && results.multiHandedness) {
+        for (let index = 0; index < results.multiHandLandmarks.length; index++) {
+            const classification = results.multiHandedness[index];
+            const isRightHand = classification.label === 'Right';
+            const landmarks = results.multiHandLandmarks[index];
+            drawingUtils.drawConnectors(canvasCtx, landmarks, mpHands.HAND_CONNECTIONS, { color: isRightHand ? '#00FF00' : '#FF0000' });
+            drawingUtils.drawLandmarks(canvasCtx, landmarks, {
+                color: isRightHand ? '#00FF00' : '#FF0000',
+                fillColor: isRightHand ? '#FF0000' : '#00FF00',
+                radius: (data) => {
+                    return drawingUtils.lerp(data.from.z, -0.15, .1, 10, 1);
+                }
+            });
+        }
+    }
+    canvasCtx.restore();
+}
